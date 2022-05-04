2d1f82388f72941179d1fa6bcdb909b379a53625
Matheus Dresch <contato.matheusdresch@gmail.com>
2022-05-03 22:48:12 -0300

    sc

--- index.js
+++ index.js
@@ -4,38 +4,19 @@ const discordTag = document.querySelector('#discordTag')
 const warningSuccess = document.querySelector('#copywarnSuccess')
 const warningError = document.querySelector('#copywarnError')
 
-const isMobile = screen.width < 480
-
 discord.addEventListener('click', async () => {
-    if(!isMobile) {
-        navigator.clipboard.writeText(`${discordTag.innerText}`).then(() => {
-            warningSuccess.classList.add('warn--vis')
-    
-            setTimeout(() => {
-                warningSuccess.classList.remove('warn--vis')
-            }, 2000)
-        }).catch(err => {
-            warningError.classList.add('warn--vis')
-    
-            console.log(err);
-            setTimeout(() => {
-                warningError.classList.remove('warn--vis')
-            }, 2000)
-        })
-    } else {
-        navigator.share(`${discordTag.innerText}`).then(() => {
-            warningSuccess.classList.add('warn--vis')
-    
-            setTimeout(() => {
-                warningSuccess.classList.remove('warn--vis')
-            }, 2000)
-        }).catch(err => {
-            warningError.classList.add('warn--vis')
-    
-            console.log(err);
-            setTimeout(() => {
-                warningError.classList.remove('warn--vis')
-            }, 2000)
-        })
-    }
+    navigator.clipboard.writeText(`${discordTag.innerText}`).then(() => {
+        warningSuccess.classList.add('warn--vis')
+
+        setTimeout(() => {
+            warningSuccess.classList.remove('warn--vis')
+        }, 2000)
+    }).catch(err => {
+        warningError.classList.add('warn--vis')
+
+        console.log(err);
+        setTimeout(() => {
+            warningError.classList.remove('warn--vis')
+        }, 2000)
+    })
 })
